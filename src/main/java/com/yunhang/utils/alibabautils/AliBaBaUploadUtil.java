a6ec3d911e11b4d7566260df326b1e05b44d5a9d
yuhuatian520 <928734613@qq.com>
2019-10-22 10:31:26 +0800

    解决上传速度慢

--- src/main/java/com/yunhang/utils/alibabautils/AliBaBaUploadUtil.java
+++ src/main/java/com/yunhang/utils/alibabautils/AliBaBaUploadUtil.java
@@ -12,6 +12,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 import static com.yunhang.utils.alibabautils.AliBaBaConst.iPAddressInfo;
 
@@ -28,7 +29,6 @@ import static com.yunhang.utils.alibabautils.AliBaBaConst.iPAddressInfo;
 public class AliBaBaUploadUtil {
 
     public String uploadFileToOss(MultipartFile file) throws IOException {
-
         if (file != null) {
             // 判断上传图片类型是否合法
             Boolean sign = FileUtils.detectionFileInfo(file);
@@ -41,25 +41,28 @@ public class AliBaBaUploadUtil {
                fileInput.write(file.getBytes());
                fileInput.close();
                file.transferTo(newFile);
-            // 获取文件的类型
-            String contentType = originalFilename.indexOf(".") != -1 ? originalFilename.substring(originalFilename.lastIndexOf(".") + 1, originalFilename.length()) : null;
-            // 生成新的文件名 （取当前时间戳和六位随机数拼接成文件名）
-            String newFileName =UUID.randomUUID().toString().trim().replace("-","") + "." + contentType;
-               // 创建OSSClient实例。
-               OSS ossClient = new OSSClientBuilder().build(AliBaBaConst.endpoint,AliBaBaConst.accessKeyId,AliBaBaConst.secretAccessKey);
-               PutObjectRequest putObjectRequest = new PutObjectRequest(AliBaBaConst.bucketName, newFileName,newFile);
-               ossClient.putObject(putObjectRequest);
-                    // 关闭OSSClient。
-               ossClient.shutdown();
-               if (newFile.exists()){
-                   newFile.delete();
-               }else {
-                   log.info("文件不存在!");
-               }
+               // 获取文件的类型
+               String contentType = originalFilename.indexOf(".") != -1 ? originalFilename.substring(originalFilename.lastIndexOf(".") + 1, originalFilename.length()) : null;
+               // 生成新的文件名 （取当前时间戳和六位随机数拼接成文件名）
+               String newFileName = UUID.randomUUID().toString().trim().replace("-", "") + "." + contentType;
+               CompletableFuture.runAsync(()-> {
+                   // 创建OSSClient实例。
+                   OSS ossClient = new OSSClientBuilder().build(AliBaBaConst.endpoint, AliBaBaConst.accessKeyId, AliBaBaConst.secretAccessKey);
+                   PutObjectRequest putObjectRequest = new PutObjectRequest(AliBaBaConst.bucketName, newFileName, newFile);
+                   ossClient.putObject(putObjectRequest);
+                   // 关闭OSSClient。
+                   ossClient.shutdown();
+                   if (newFile.exists()) {
+                       newFile.delete();
+                   } else {
+                       log.info("文件不存在!");
+                   }
+               });
                return iPAddressInfo+newFileName;
            }
         }
         return "";
+
     }
 }
 
